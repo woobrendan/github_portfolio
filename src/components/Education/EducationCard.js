8393799bf2ee9255d70179a9b94718a4f59057b8
Brendan Woo <woo.brendan@gmail.com>
2022-11-08 13:05:55 -0800

    import cardmedia. add cardmedia component, set image to imageURL, set height. remove div for img container, add school year to school info div

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -1,17 +1,20 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, CardMedia } from "@mui/material";
 
 function EducationCard(props) {
   const school = props.schoolDetails;
   return (
     <Card sx={{ maxWidth: 700 }} >
+      <CardMedia
+          component="img"
+          image={school.imageURL}
+          alt={school.schoolName}
+          height="200"
+        />
         <CardContent >
           <div className="education_header">
-            <div className="education_img_container">
-              <img src={school.imageURL} alt={school.schoolName}/>
-            </div>
             <div className="school_info">
-              <h1>{school.schoolName}</h1>
-              <p>{school.degree}</p>
+              <h1>{school.degree}</h1>
+              <h3>{school.year}</h3>
             </div>
           </div>
           <div className="education_description">
