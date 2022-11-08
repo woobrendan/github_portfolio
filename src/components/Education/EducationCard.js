58d029b66fd8b33228c2ba47836c67f3f8786e17
Brendan Woo <woo.brendan@gmail.com>
2022-11-08 12:33:19 -0800

    import card, cardcontent. wrap component in same. remove div education_card, set maxwidth 700 on card

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -1,23 +1,24 @@
-// degree, description, image url, school, year
-//
+import { Card, CardContent } from "@mui/material";
 
 function EducationCard(props) {
   const school = props.schoolDetails;
   return (
-    <div className="education_card">
-      <div className="education_header">
-        <div className="education_img_container">
-          <img src={school.imageURL} alt={school.schoolName}/>
-        </div>
-        <div className="school_info">
-          <h1>{school.schoolName}</h1>
-          <p>{school.degree}</p>
-        </div>
-      </div>
-      <div className="education_description">
-        <p>{school.description}</p>
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 700 }} >
+        <CardContent >
+          <div className="education_header">
+            <div className="education_img_container">
+              <img src={school.imageURL} alt={school.schoolName}/>
+            </div>
+            <div className="school_info">
+              <h1>{school.schoolName}</h1>
+              <p>{school.degree}</p>
+            </div>
+          </div>
+          <div className="education_description">
+            <p>{school.description}</p>
+          </div>   
+      </CardContent>
+    </Card>
   )
 } 
 
