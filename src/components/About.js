c5075d6e70986f51b05d2529b5ec5610a7928b7f
Brendan Woo <woo.brendan@gmail.com>
2022-11-09 11:24:25 -0800

    change typography h4 into standard h4 element. wrap intro in container and change from typography to p tags

--- src/components/About.js
+++ src/components/About.js
@@ -25,26 +25,26 @@ export default function AboutMe() {
           id="About-img"
         />
         <Card sx={{ maxWidth: 700 }} id="about-description">
-            <CardContent >
-              <BoyIcon fontSize="large"/>
-              <Typography gutterBottom variant="h4" component="div">
-                Life of Brendan
-              </Typography>
-              <Typography variant="body2" color="text.secondary" align="left">
-              Hello! I am an aspiring Software Developer transitioning my career from professional sports. After 8 great years of working in sports car racing, I decided to make a change and took a Full Stack Web Development Bootcamp at Lighthouse Labs where in the span of 12 weeks I went from knowing nothing about coding to being able to develop my own functioning app.
-              <br></br>
-              <br></br>
+          <CardContent>
+            <BoyIcon fontSize="large"/>
+            <h4>Life of Brendan Woo</h4>
+            <div className="intro_container">
+              <p>Hello and welcome to my portfolio!</p>
+              <p>I am an aspiring Software Developer transitioning my career from professional sports. After 8 great years of working in sports car racing, I decided to make a change and took a Full Stack Web Development Bootcamp at Lighthouse Labs where in the span of 12 weeks I went from knowing nothing about coding to being able to develop my own functioning app.
+              </p>
+              <p>
               After my bootcamp I was able to identify some areas of high stress and inefficiency at my racing job, and decided to build my own application to see if I could make work flow smoother. I designed my app Podium Assistant that removed the rush of preparing a last minute podium script and instead allowed for easily selectable winners that populated a script with all required information with the click of a button.
-              <br></br>
-              <br></br>
+              </p>
+              <p>
               I am super satisfied with the improvement to work life quality that my app provided to my colleagues and myself, and am excited for the next opportunity to contribute within the industry.
-              <br></br> 
-              <br></br> 
+              </p>
+              <p>
               If you would like to learn more or collaborate on future opportunities, please feel free to connect with me.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Education />
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+        <Education />
       </div>
     </div>
   )
