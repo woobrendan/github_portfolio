8f2b558242f2104c97dfb6d9ee1ac8c97d6a1958
Brendan Woo <woo.brendan@gmail.com>
2022-11-09 12:18:48 -0800

    add key value name to each obj

--- src/seeds/images.js
+++ src/seeds/images.js
@@ -1,14 +1,18 @@
 export const sliderData = [
   {
+    name: 'SRO Motorsports America',
     image: 'https://www.gt4-america.com/timthumb.php?w=1600&src=%2Fimages%2F2020-03-20-covid-update.jpg'
   },
   {
+    name: 'IRB',
     image: 'https://www.lehallaw.com/wp-content/uploads/2020/11/immigration-refugee-board-canada.jpg'
   },
   {
+    name: 'Toronto Blue Jays',
     image: 'https://www.mlbstatic.com/team-logos/share/141.jpg'
   },
   {
+    name: 'Freedom Mobile',
     image: 'https://southcentremall.com/wp-content/uploads/2021/11/d1617b6125629b6e350523cfc6988ec1bcce12bb.png'
   }
 ]
