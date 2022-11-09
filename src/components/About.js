4f406984bed7793eb39207bc920d0d0bf17d0a6b
Brendan Woo <woo.brendan@gmail.com>
2022-11-09 11:42:20 -0800

    wrap card and img in container intro_header. change super statisfied to extremely

--- src/components/About.js
+++ src/components/About.js
@@ -11,9 +11,7 @@ export default function AboutMe() {
   return (
     <div id="About-container">
       <div className="About-cards">
-        <Typography variant="h3" gutterBottom component="div">
-          About Me
-        </Typography>
+        <div className="intro_header">
         <Box
           component="img"
           sx={{
@@ -36,7 +34,7 @@ export default function AboutMe() {
               After my bootcamp I was able to identify some areas of high stress and inefficiency at my racing job, and decided to build my own application to see if I could make work flow smoother. I designed my app Podium Assistant that removed the rush of preparing a last minute podium script and instead allowed for easily selectable winners that populated a script with all required information with the click of a button.
               </p>
               <p>
-              I am super satisfied with the improvement to work life quality that my app provided to my colleagues and myself, and am excited for the next opportunity to contribute within the industry.
+              I am extremely satisfied with the improvement to work life quality that my app provided to my colleagues and myself, and am excited for the next opportunity to contribute within the industry.
               </p>
               <p>
               If you would like to learn more or collaborate on future opportunities, please feel free to connect with me.
@@ -44,6 +42,7 @@ export default function AboutMe() {
             </div>
           </CardContent>
         </Card>
+        </div>
         <Education />
       </div>
     </div>
