031cbee0c3af817de8ca392d0e41a8cfcad384da
Brendan Woo <woo.brendan@gmail.com>
2022-11-11 11:22:42 -0800

    create seed file for job experience

---
+++ src/seeds/experience.js
@@ -0,0 +1,20 @@
+export default experience =  [
+  {
+    company: "SRO Motorsports America",
+    location: "Various, USA",
+    positions: [
+      {
+        title: "Team Relations Coordinator",
+        year: "Oct 2016 - Oct 2022",
+        duties: [
+          "Maintain constant communication with clients and partners through phone calls, meetings and emails to ensure strong relationships and timely assistance",
+          "Ensure the success of events by planning and managing operations, delegating tasks for prompt setup",
+          "Prepare various official documents and coordinate their distribution to constituents",
+          "Advise on marketing plans for partnership activations, such as branding, car corrals, car parades, autograph sessions, and more, improving the overall fan experience",
+          "Manage budget for vendors and operations, reducing spending for each season"
+        ]
+      }
+    ]
+  }
+]
+
