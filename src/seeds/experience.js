49f6a77d7bd9b1a90a6b63edb57ed213bb516551
Brendan Woo <woo.brendan@gmail.com>
2022-11-11 11:29:02 -0800

    add other job positions with SRO

--- src/seeds/experience.js
+++ src/seeds/experience.js
@@ -13,6 +13,24 @@ export default experience =  [
           "Advise on marketing plans for partnership activations, such as branding, car corrals, car parades, autograph sessions, and more, improving the overall fan experience",
           "Manage budget for vendors and operations, reducing spending for each season"
         ]
+      },
+      {
+        title: "Video Technician",
+        year: "Jan 2016 - Oct 2016",
+        duties: [
+          "Created a new process to structure and expedite video submissions, reducing wait times and increasing efficiency by 29%",
+          "Coordinated in-car cameras with all competition teams to ensure their compliance with sporting regulations and with marketing and TV requirements",
+          "Acquired in-car video for processing, reacting to changing demands of competition stewards and TV production crew"
+        ]
+      },
+      {
+        title: "Marketing & Communications Intern",
+        year: "May 2014 - Dec 2015",
+        duties: [
+          "Ensured successful hosting of racing events throughout North America by collaborating with the Marketing & Communications team",
+          "Assisted the migration to more modern marketing practices through social media platforms resulting in increased awareness and social traffic",
+          "Facilitated the distribution of marketing materials to both teams and the public, including over 500 copies of print media per event"
+        ]
       }
     ]
   }
