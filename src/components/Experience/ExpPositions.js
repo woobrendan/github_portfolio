bbc551f25277a0ef5a3b9e1acfce46da7c5e1910
Brendan Woo <woo.brendan@gmail.com>
2022-11-14 11:42:38 -0800

    remove hard coded data, add in mappedDuties in ul. add key to mappedDuties

--- src/components/Experience/ExpPositions.js
+++ src/components/Experience/ExpPositions.js
@@ -1,28 +1,20 @@
 function ExperienceItem(props) {
-  
+
   const position = props.position
   const mappedDuties = position.duties.mappedDuties((duty, index) => (
-    <li>
+    <li key={index}>
       {duty}
     </li>
   ))
  return (
    <>
-    <h3>{position.title}</h3>
     <div className="work-info">
-        <p>{position.year}</p>
-      </div>
-      <ul className="job-description-list">
-        <li><span>
-          Worked a casual contract supporting multiple teams and managers with administrative duties such as  facilitation of official documents, taking meeting notes and manage member absence line
-        </span></li>
-        <li><span>
-          Coordinated manager's schedules, ensuring days and meetings are organized
-        </span></li>
-        <li><span>
-          Facilitated member hearing schedules and caseloads, updating HR software as required
-        </span></li>
-      </ul>
+      <h3>{position.title}</h3>
+      <p>{position.year}</p>
+    </div>
+    <ul className="job-description-list">
+      {mappedDuties}
+    </ul>
    </>
  )
 }
