d99e5fa51db2cb2fdf91e812e2fded515bf1f15b
Brendan Woo <woo.brendan@gmail.com>
2022-11-14 11:40:26 -0800

    remove hardcoded data, replace with mappedJobs. add in location under company

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -20,24 +20,13 @@ function ExperienceCard(props) {
         />
         <CardContent>
           <h2>{experience.company}</h2>
+          <h5>{experience.location}</h5>
           <Typography gutterBottom variant="subtitle1" component="div">
             Assistant to Coordinating Member
           </Typography>
           <div className="work-info">
-            <p>Location: Toronto, ON</p>
-            <p>Sept 2021 - Dec 2021</p>
-          </div>
-          <ul className="job-description-list">
-            <li><span>
-              Worked a casual contract supporting multiple teams and managers with administrative duties such as  facilitation of official documents, taking meeting notes and manage member absence line
-            </span></li>
-            <li><span>
-              Coordinated manager's schedules, ensuring days and meetings are organized
-            </span></li>
-            <li><span>
-              Facilitated member hearing schedules and caseloads, updating HR software as required
-            </span></li>
-          </ul>
+            {mappedJobs}
+          </div>           
         </CardContent>
       </Card>
     </>
