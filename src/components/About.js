bc89d73c3694de8a4750d8382e5b232ea2cfd3fe
Brendan Woo <woo.brendan@gmail.com>
2022-11-16 12:29:41 -0800

    remove div about_cards. remove unused imports for mui and boyIcon

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,4 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import '../Styles/about.scss'
-import BoyIcon from '@mui/icons-material/Boy';
 import Box from '@mui/material/Box';
 import pwc from '../images/PWC.jpg'
 import Education from './Education/Education';
@@ -11,25 +8,23 @@ import AboutDescription from './AboutDescription';
 export default function AboutMe() {
   return (
     <div id="About-container">
-      <div className="About-cards">
-        <div className="intro_header">
-          <div className="intro_left">
-            <Box
-              component="img"
-              sx={{
-                width: 300,
-                height: 300,
-              }}
-              alt="SRO Portrait"
-              src={pwc}
-              id="About-img"
-            />
-            <ImageSlider />
-          </div>
-          <AboutDescription />
+      <div className="intro_header">
+        <div className="intro_left">
+          <Box
+            component="img"
+            sx={{
+              width: 300,
+              height: 300,
+            }}
+            alt="SRO Portrait"
+            src={pwc}
+            id="About-img"
+          />
+          <ImageSlider />
         </div>
-        <Education />
+        <AboutDescription />
       </div>
+      <Education />
     </div>
   )
 }
