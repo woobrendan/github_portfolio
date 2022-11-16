87958160db4680746f86bce3005947fd2b8e4f86
Brendan Woo <woo.brendan@gmail.com>
2022-11-16 12:06:43 -0800

    remove methods mappedSkills and mappedSkillList. remove import for typogaphy and consolidate Mui imports for card and card content

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,33 +1,9 @@
-import { Typography } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { Card, CardContent } from "@mui/material";
 import { skills } from "../../seeds/skill_info";
 import '../../Styles/skills.scss'
 import SkillList from "./SkillList";
 
 function Skills() {
-  console.log('skills', skills)
-  const mappedSkillList = (someArray) => {
-    const skillList = someArray.map((skill, index) => (
-        <li key={index}>
-          <span>{skill}</span>
-        </li>
-    ))
-    return skillList;
-  }
-
-  const mappedSkills = skills.map((skill, index) => (
-    <Card key={index}>
-      <CardContent>
-        <Typography gutterBottom variant="h4" component="div">
-          {skill.title}
-        </Typography>
-        <ul className="skill-info-list">
-          {mappedSkillList(skill.info)}
-        </ul>
-      </CardContent>
-    </Card>
-  ));
 
   return (
     <>
