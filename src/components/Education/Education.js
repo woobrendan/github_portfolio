b2f8bd54c200c8857874874cf37297c146dc8a30
Brendan Woo <woo.brendan@gmail.com>
2022-11-17 11:07:54 -0800

    move mappedEducation logic into component return. Import new education styling sheet

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,32 +1,19 @@
-import WorkIcon from '@mui/icons-material/Work';
 import education from '../../seeds/education.json'
 import EducationCard from './EducationCard';
+import '../../Styles/education.scss'
 
 function Education() {
   
-  const mappedEducation = education.map((school) => (
-    <EducationCard schoolDetails={school} key={school.schoolName}/>
-  ))
   return (
     <>
       <h1>Education</h1>
-        <div className="Card-list">
-          {mappedEducation}
-        {/* <Card sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <WorkIcon />
-            <Typography gutterBottom variant="h4" component="div">
-              Working Experience
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              7+ Years in Professional Sports
-            </Typography>
-            <ul>
-              <li>SRO Motorsports America</li>
-              <li>Toronto Blue Jays</li>
-            </ul>
-          </CardContent>
-        </Card> */}
+        <div className="education_card_list">
+          {education.map((school) => (
+            <EducationCard 
+              schoolDetails={school} 
+              key={school.schoolName}
+            />
+          ))}
       </div>
     </>
   )
