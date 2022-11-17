c5f43d1e293e8c08aea8f4d7181e105e0fb4a8db
Brendan Woo <woo.brendan@gmail.com>
2022-11-17 11:01:53 -0800

    add sx prop to card component, set width to 650

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -10,7 +10,7 @@ function Skills() {
       <h1>Skills</h1>
       <div id="skills-list">
         {skills.map((skill, index) => (
-          <Card key={index}>
+          <Card key={index} sx={{ width: 650 }}>
             <CardContent>
                 <div className="skill_card" key={skill.title}>
                   <h2>{skill.title}</h2>
