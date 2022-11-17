5e0648ca9364d2fe252d4d0531d20bbca3e2a429
Brendan Woo <woo.brendan@gmail.com>
2022-11-17 11:13:30 -0800

    fx indentation. Change h4 from description to details. Make circle icons medium instead of large

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -6,35 +6,37 @@ import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 function EducationCard(props) {
   
   const [showDescription, setShowDescription] = useState(false);
+
   const school = props.schoolDetails;
 
-  const handleToggle = () => setShowDescription(!showDescription)
+  const handleToggle = () => setShowDescription(!showDescription);
+
   return (
     <Card sx={{ width: 650 }} className="education_card">
       <CardMedia
-          component="img"
-          image={school.imageURL}
-          alt={school.schoolName}
-          height="200"
-        />
-        <CardContent >
-          {!showDescription && <div className="school_info">
-            <h1>{school.degree}</h1>
-            <h3>{school.year}</h3>
+        component="img"
+        image={school.imageURL}
+        alt={school.schoolName}
+        height="200"
+      />
+      <CardContent >
+        {!showDescription && <div className="school_info">
+          <h1>{school.degree}</h1>
+          <h3>{school.year}</h3>
+        </div>}
+        {showDescription && 
+          <div className="education_description">
+            <p>{school.description}</p>
           </div>}
-          {showDescription && 
-            <div className="education_description">
-              <p>{school.description}</p>
-            </div>}
-          <div className="description_toggle">
-            <h4>Description</h4>
-            <Button onClick={()=> handleToggle()}>
-              {!showDescription && 
-                <AddCircleOutlineIcon fontSize="large" />}
-              {showDescription && 
-                <RemoveCircleOutlineIcon fontSize="large" />}
-            </Button>  
-          </div> 
+        <div className="description_toggle">
+          <h4>Details</h4>
+          <Button onClick={()=> handleToggle()}>
+            {!showDescription && 
+              <AddCircleOutlineIcon fontSize="medium" />}
+            {showDescription && 
+              <RemoveCircleOutlineIcon fontSize="medium" />}
+          </Button>  
+        </div> 
       </CardContent>
     </Card>
   )
