048126e439f589bad6082c385528042ec8435db3
Brendan Woo <woo.brendan@gmail.com>
2022-11-18 13:40:44 -0800

    add method scrollUp and apply to onclick for icon button

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import ArrowCircleUpTwoToneIcon from '@mui/icons-material/ArrowCircleUpTwoTone';
 import { IconButton } from '@mui/material'
 
 function ToTop() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 50,
+      behavior: "smooth"
+    })
+  }
   return (
     <div className="to_top">
-      <IconButton id="To-Top-Button">
+      <IconButton onClick={scrollUp} id="To-Top-Button">
         <ArrowCircleUpTwoToneIcon  />
       </IconButton>
     </div>
