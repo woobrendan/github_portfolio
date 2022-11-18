65cabf99f28144d92a27d2b363c5ea3897f74a0e
Brendan Woo <woo.brendan@gmail.com>
2022-11-18 13:03:37 -0800

    import arrow circle icon, import iconButtons from MUI. render within component

--- src/components/ToTop.js
+++ src/components/ToTop.js
@@ -1,9 +1,14 @@
+import ArrowCircleUpTwoToneIcon from '@mui/icons-material/ArrowCircleUpTwoTone';
+import { IconButton } from '@mui/material'
+
 function ToTop() {
   return (
     <div className="to_top">
-
+      <IconButton id="To-Top-Button">
+        <ArrowCircleUpTwoToneIcon  />
+      </IconButton>
     </div>
   )
 }
 
-export default ToTop.js
+export default ToTop
