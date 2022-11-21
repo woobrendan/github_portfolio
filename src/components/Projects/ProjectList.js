8a0b1052f34f136ac309f355ea281964b2feb72b
Brendan Woo <woo.brendan@gmail.com>
2022-11-21 12:22:33 -0800

    add mapped TestProject constant to render new project styling. return in component

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,15 +1,30 @@
 import { projects } from '../../seeds/projectInfo.js'
 import Project from './ProjectCard'
+import ProjectSide from './ProjectSide.js'
 
 function ProjectList() {
   const mappedProjects = projects.map((project, index) => (
     <Project project={project} key={index}/>
   ))
+
+  const testProject = projects.map((project, index) => (
+    <ProjectSide
+      project={project} 
+      key={index}
+      index={index}
+    
+    />
+  ))
+
+
   return (
     <>
       <h1>Projects</h1>
       <div id="project-list">
         {mappedProjects}
+      </div>
+      <div className="test">
+        {testProject}
       </div>
     </>
   )
