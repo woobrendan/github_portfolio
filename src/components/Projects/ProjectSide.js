2a33df60c6181ae401dac888f4f0e3aa4a7e3d7a
Brendan Woo <woo.brendan@gmail.com>
2022-11-21 14:15:35 -0800

    move each div into its own constant, and render constant in component

--- src/components/Projects/ProjectSide.js
+++ src/components/Projects/ProjectSide.js
@@ -3,20 +3,28 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 
 function ProjectSide(props) {
   const project = props.project
+  const projectImg = (
+    <div className="project_img">
+      <img src={project.gif} alt={project.name} />
+    </div>
+  )
+  const projectDetails = (
+    <div className="project_info">
+      <h5>{project.name}</h5>
+      <p>{project.description}</p>
+      <p>Frameworks: {project.frameworks}</p>
+      <Button variant="outlined" href={project.link} startIcon={<GitHubIcon />}>
+        Code
+      </Button>
+    </div>
+  )
+  
+
 
   return (
     <section className="project_side_view">
-      <div className="project_img">
-        <img src={project.gif} alt={project.name} />
-      </div>
-      <div className="project_info">
-        <h5>{project.name}</h5>
-        <p>{project.description}</p>
-        <p>Frameworks: {project.frameworks}</p>
-        <Button variant="outlined" href={project.link} startIcon={<GitHubIcon />}>
-          Code
-        </Button>
-      </div>
+      {projectImg}
+      {projectDetails}
     </section>
   )
 }
