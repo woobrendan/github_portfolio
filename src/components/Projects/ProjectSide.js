7e3c024951ca2cad5419089454cf41f97070cbe0
Brendan Woo <woo.brendan@gmail.com>
2022-11-21 12:09:37 -0800

    add in info for project_info div. change wrapper to be section element instead of div. import button and githubicon

--- src/components/Projects/ProjectSide.js
+++ src/components/Projects/ProjectSide.js
@@ -1,8 +1,22 @@
+import { Button } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 function ProjectSide(props) {
   const project = props.project
 
   return (
     <section className="project_side_view">
+      <div className="project_img">
+
+      </div>
+      <div className="project_info">
+        <h5>{project.name}</h5>
+        <p>{project.description}</p>
+        <p>Frameworks: {project.frameworks}</p>
+        <Button variant="outlined" href={project.link} startIcon={<GitHubIcon />}>
+          Code
+        </Button>
+      </div>
     </section>
   )
 }
