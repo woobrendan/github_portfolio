6f25cf95048c31bfe8eeba36fc334dfc1a808f11
Brendan Woo <woo.brendan@gmail.com>
2022-11-22 13:15:55 -0800

    create handleNextClick for next and prev icon clicks

--- src/components/Experience/ExpCarousel.js
+++ src/components/Experience/ExpCarousel.js
@@ -7,6 +7,12 @@ function ExpCarousel() {
   const [current, setCurrent] = useState(0);
   const length = experience.length;
 
+  const handleNextClick = (next) => {
+    next === 'next' 
+      ? current === length - 1 ? setCurrent(0) : setCurrent(current + 1)
+      : current === 0 ? setCurrent(length - 1) : setCurrent(current - 1) 
+  }
+
   const experienceCards = experience.map((job, index) => (
     <div className={index === current ? 'slide_active' : 'slide'} key={job.company}>
       {index === current && <ExperienceCard experience={job} />}
