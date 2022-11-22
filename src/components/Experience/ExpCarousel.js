6bef595a8351ec5dc275b980ee91edb20014b1f3
Brendan Woo <woo.brendan@gmail.com>
2022-11-22 13:10:08 -0800

    create const experienceCards to render cards, add constant for length. import experience seeds and experiencecard component

--- src/components/Experience/ExpCarousel.js
+++ src/components/Experience/ExpCarousel.js
@@ -1,8 +1,17 @@
 import { useState } from 'react'
+import { experience } from '../../seeds/experience';
+import ExperienceCard from './ExperienceCard';
 
 function ExpCarousel() {
 
   const [current, setCurrent] = useState(0);
+  const length = experience.length;
+
+  const experienceCards = experience.map((job, index) => (
+    <div className={index === current ? 'slide_active' : 'slide'} key={job.company}>
+      {index === current && <ExperienceCard experience={job} />}
+    </div>
+  ))
   
   return (
     <>
