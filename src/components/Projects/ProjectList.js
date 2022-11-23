2c4467541af33663e511650c09e48f4c96ca3a0b
Brendan Woo <woo.brendan@gmail.com>
2022-11-23 12:09:51 -0800

    remove Project component import. import project style sheet. remove testProject constant for mapped cards, and render inside component

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,24 +1,20 @@
 import { projects } from '../../seeds/projectInfo.js'
-// import Project from './ProjectCard'
 import ProjectSide from './ProjectSide.js'
+import '../../Styles/project.scss'
 
 function ProjectList() {
-
-  const testProject = projects.map((project, index) => (
-    <ProjectSide
-      project={project} 
-      key={index}
-      index={index + 1}
-    
-    />
-  ))
-
 
   return (
     <>
       <h1>Projects</h1>
       <section id="project-list" >
-        {testProject}
+        {projects.map((project, index) => (
+          <ProjectSide
+            project={project} 
+            key={index}
+            index={index + 1}
+          />
+        ))}
       </section>
     </>
   )
