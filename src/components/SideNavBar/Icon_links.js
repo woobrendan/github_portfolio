6dc092563d94427c33f963b1069a825941a5fe0a
Brendan Woo <woo.brendan@gmail.com>
2022-11-24 13:59:52 -0800

    import getCOmponent as props. pass to onclick handler for each icon

--- src/components/SideNavBar/Icon_links.js
+++ src/components/SideNavBar/Icon_links.js
@@ -2,32 +2,37 @@ import { Person, Email, WorkHistory, StickyNote2, Construction } from '@mui/icon
 import { useState } from 'react'
 
 
-export default function IconLinks() {
+export default function IconLinks({ getComponent }) {
 
-  const [selected, setSelected] = useState('')
+  const [selected, setSelected] = useState('');
+
+  const getValue = (event) => {
+    console.log('event', event.target.name)
+    setSelected(event.target.name)
+  }
 
   return(
     <div className="icon-list">
       <nav>
         <ul>
           <li>
-            <a href="#About-container" className="about-icon" name="About" onClick={getValue}>
+            <a href="#About-container" className="about-icon" name="About" onClick={(e) => getComponent(e.target.name)}>
               <Person fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#project-list" className="project-icon" name="Project" onClick={getValue}>
+            <a href="#project-list" className="project-icon" name="Project" onClick={(e) => getComponent(e.target.name)}>
               <StickyNote2 fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#skills-list" className="skills-icon"  name="Skills" onClick={getValue}>
+            <a href="#skills-list" className="skills-icon"  name="Skills" onClick={(e) => getComponent(e.target.name)}>
               <Construction fontSize="large"/>
             </a>
           </li>
           <li>
             <a href="#Experience" className="work-icon"
-            name="Experience" onClick={getValue}>
+            name="Experience" onClick={(e) => getComponent(e.target.name)}>
               <WorkHistory fontSize="large"/>
             </a>
           </li>
