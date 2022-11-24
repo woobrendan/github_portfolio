397c0b0111e9e0b76e9a55d5d9450cfab8a7d321
Brendan Woo <woo.brendan@gmail.com>
2022-11-24 11:32:19 -0800

    remove import statement for experience component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './Styles/App.scss';
 import AboutMe from './components/About/About';
-import Experience from './components/Experience/experience';
 import Sidebar from './components/SideNavBar/Sidebar';
 import ProjectList from './components/Projects/ProjectList';
 import Skills from './components/Skills/Skills';
