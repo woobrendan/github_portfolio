fa778992f27df00cf2d015449f4347c9d89d96e1
Brendan Woo <woo.brendan@gmail.com>
2022-11-24 13:52:14 -0800

    create state selected. add name props to each a tag, and onclick handler passing getValue

--- src/components/SideNavBar/Icon_links.js
+++ src/components/SideNavBar/Icon_links.js
@@ -1,28 +1,33 @@
 import { Person, Email, WorkHistory, StickyNote2, Construction } from '@mui/icons-material'
+import { useState } from 'react'
 
 
 export default function IconLinks() {
+
+  const [selected, setSelected] = useState('')
+
   return(
     <div className="icon-list">
       <nav>
         <ul>
           <li>
-              <a href="#About-container" className="about-icon">
-                <Person fontSize="large"/>
-              </a>
+            <a href="#About-container" className="about-icon" name="About" onClick={getValue}>
+              <Person fontSize="large"/>
+            </a>
           </li>
           <li>
-              <a href="#project-list" className="project-icon">
-                <StickyNote2 fontSize="large"/>
-              </a>
+            <a href="#project-list" className="project-icon" name="Project" onClick={getValue}>
+              <StickyNote2 fontSize="large"/>
+            </a>
           </li>
           <li>
-              <a href="#skills-list" className="skills-icon">
-                <Construction fontSize="large"/>
-              </a>
+            <a href="#skills-list" className="skills-icon"  name="Skills" onClick={getValue}>
+              <Construction fontSize="large"/>
+            </a>
           </li>
           <li>
-            <a href="#Experience" className="work-icon">
+            <a href="#Experience" className="work-icon"
+            name="Experience" onClick={getValue}>
               <WorkHistory fontSize="large"/>
             </a>
           </li>
