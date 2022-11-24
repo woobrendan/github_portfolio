020208bfa845a07a9fdf47c82c079fe8f4115b23
Brendan Woo <woo.brendan@gmail.com>
2022-11-24 12:04:56 -0800

    import and create usestate for component. create method currentPage to return component based on state

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ import Skills from './components/Skills/Skills';
 import Education from './components/Education/Education';
 import ToTop from './components/ToTop';
 import ExpCarousel from './components/Experience/ExpCarousel';
+import { useState } from 'react';
 
 function App() {
+  const [component, setComponent] = useState('About');
+
+  const currentPage = (page) => {
+    switch(page) {
+      case 'About': return <AboutMe />
+      case 'Education': return <Education />
+      case 'Projects': return <ProjectList />
+      case 'Skills': return <Skills />
+      case 'Experience': return <ExpCarousel />
+    }
+  }
+
   return (
     <div className="App">
       <h1>Brendan Woo</h1>
       <h3>Full Stack Developer</h3>
       <Sidebar />
-      <AboutMe />
+      {currentPage(component)}
+      {/* <AboutMe />
       <Education />
       <ProjectList />
       <Skills />
-      <ExpCarousel />
+      <ExpCarousel /> */}
       {/* <div id="Contact">
         Email Me
       </div> */}
