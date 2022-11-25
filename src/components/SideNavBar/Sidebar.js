c5dac7d4e21db721f1abb30da6a362619c2701cb
Brendan Woo <woo.brendan@gmail.com>
2022-11-25 13:57:13 -0800

    add currentComponent to props import, add as prop to pass down to iconLinks

--- src/components/SideNavBar/Sidebar.js
+++ src/components/SideNavBar/Sidebar.js
@@ -1,11 +1,14 @@
 import IconLinks from "./Icon_links"
 import '../../Styles/nav-bar.scss'
 
-export default function Sidebar({ getComponent }) {
+export default function Sidebar({ getComponent, currentComponent }) {
 
   return (
     <div className="nav-bar">
-      <IconLinks getComponent={getComponent}/>
+      <IconLinks 
+        getComponent={getComponent}
+        currentComponent={currentComponent}
+      />
     </div>
   )
 }
