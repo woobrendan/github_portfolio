5464994bbdf078dc5e6385000fcca2737a3af58b
Brendan Woo <woo.brendan@gmail.com>
2022-11-25 13:41:42 -0800

    remove href from all a tags

--- src/components/SideNavBar/Icon_links.js
+++ src/components/SideNavBar/Icon_links.js
@@ -7,28 +7,27 @@ export default function IconLinks({ getComponent }) {
       <nav>
         <ul>
           <li>
-            <a href="#About-container" className="home-icon" name="Home" onClick={(e) => getComponent(e.target.name)}>
+            <a className="home-icon" name="Home" onClick={(e) => getComponent(e.target.name)}>
               <Home fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#About-container" className="about-icon" name="About" onClick={(e) => getComponent(e.target.name)}>
+            <a className="about-icon" name="About" onClick={(e) => getComponent(e.target.name)}>
               <Person fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#project-list" className="project-icon" name="Project" onClick={(e) => getComponent(e.target.name)}>
+            <a className="project-icon" name="Project" onClick={(e) => getComponent(e.target.name)}>
               <StickyNote2 fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#skills-list" className="skills-icon"  name="Skills" onClick={(e) => getComponent(e.target.name)}>
+            <a className="skills-icon" name="Skills" onClick={(e) => getComponent(e.target.name)}>
               <Construction fontSize="large"/>
             </a>
           </li>
           <li>
-            <a href="#Experience" className="work-icon"
-            name="Experience" onClick={(e) => getComponent(e.target.name)}>
+            <a className="work-icon" name="Experience" onClick={(e) => getComponent(e.target.name)}>
               <WorkHistory fontSize="large"/>
             </a>
           </li>
