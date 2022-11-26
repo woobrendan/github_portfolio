206aa4528930ed190d819f2704e6c6d638e57a1b
Brendan Woo <woo.brendan@gmail.com>
2022-11-25 17:03:19 -0800

    add display key to all objects in sections. change selected to show section.display instead of name

--- src/components/SideNavBar/Icon_links.js
+++ src/components/SideNavBar/Icon_links.js
@@ -7,30 +7,35 @@ export default function IconLinks({ getComponent, currentComponent }) {
     {
     className: 'home-icon',
     name: 'Home',
+    display: 'HOME',
     component: (<Home fontSize="large" />),
     onClick: ((e) => getComponent(e.target.name))
     },
     {
       className: 'about-icon',
       name: 'About',
+      display: 'ABOUT',
       component: (<Person fontSize="large" />),
       onClick: ((e) => getComponent(e.target.name))
     },
     {
       className: 'project-icon',
       name: 'Project',
+      display: 'PROJECT',
       component: (<StickyNote2 fontSize="large" />),
       onClick: ((e) => getComponent(e.target.name))
     },
     {
       className: 'skills-icon',
       name: 'Skills',
+      display: 'SKILLS',
       component: (<Construction fontSize="large"/>),
       onClick: ((e) => getComponent(e.target.name))
     },
     {
       className: 'work-icon',
       name: 'Experience',
+      display: 'EXPERIENCE',
       component: (<WorkHistory fontSize="large"/>),
       onClick: ((e) => getComponent(e.target.name))
     }
@@ -43,8 +48,8 @@ export default function IconLinks({ getComponent, currentComponent }) {
         ? <a className={section.className} name={section.name} onClick={section.onClick}>
             {section.component}
           </a>
-        : <a>
-            {section.name}
+        : <a className="selected">
+            {section.display}
           </a>
       }
     </li>
