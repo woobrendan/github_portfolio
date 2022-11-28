69df5c1cce5bcc832df8911980de658eaa97864d
Brendan Woo <woo.brendan@gmail.com>
2022-11-28 11:53:01 -0800

    change wrapper from fragment to section, give className. create div home_img and home_description

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 function Home() {
   return (
-    <>
-      <h1>Hello I'm Brendan Woo</h1>
-      <h5>I am a Full Stack Developer based in Canada</h5>
-    </>
+    <section className="home_container">
+      <div className="home_img">
+        
+      </div>
+      <div className='home_description'>
+        <h1>Hello I'm Brendan Woo</h1>
+        <h5>I am a Full Stack Developer based in Canada</h5>
+      </div>
+    </section>
   )
 }
 
