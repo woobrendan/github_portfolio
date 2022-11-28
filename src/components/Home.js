c466e623d1b4ee8d3ee88aa84e22078c3e5ab18e
Brendan Woo <woo.brendan@gmail.com>
2022-11-28 13:23:28 -0800

    import style sheet and self image. render img element with src self

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
+import self from '../images/me.jpeg';
+import '../Styles/home.scss'
+
 function Home() {
   return (
     <section className="home_container">
       <div className="home_img">
-        
+        <img src={self} alt="portrait" />
       </div>
       <div className='home_description'>
         <h1>Hello I'm Brendan Woo</h1>
