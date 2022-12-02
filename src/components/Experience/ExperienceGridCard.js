8871dfcdece5b0d052a3ba641d889a4848f5614d
Brendan Woo <woo.brendan@gmail.com>
2022-12-02 12:57:10 -0800

    remove commented out boilerplate from expcard. import props getCompName, pass to onclick handler

--- src/components/Experience/ExperienceGridCard.js
+++ src/components/Experience/ExperienceGridCard.js
@@ -1,26 +1,16 @@
 import { Card, CardMedia, CardContent } from '@mui/material';
 
-function ExperienceGridCard({ experience }) {
+function ExperienceGridCard({ experience, getCompName }) {
   return (
 
     <div className="experience_grid_card">
-      <img src={experience.image} alt={experience.company} />
+      <img 
+        src={experience.image} 
+        alt={experience.company} 
+        name={experience.company}
+        onClick={(e) => getCompName(e.target.name)}
+      />
     </div>
-    // <Card sx={{ width: 200 }} className="experience_grid_card">
-    //   <CardMedia
-    //     component="img"
-    //     image={experience.image}
-    //     alt={experience.company}
-    //     sx={{ height: 195 }}
-    //   />
-    //   <CardContent>
-    //     <h2>{experience.company}</h2>
-    //     <h4>Location: {experience.location}</h4>
-    //     <div className="work-info">
-    //       {mappedJobs}
-    //     </div>           
-    //   </CardContent>
-    // </Card>
   )
 }
 
