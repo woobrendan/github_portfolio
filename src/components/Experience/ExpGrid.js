0dcdd61cb70be35e8d9f2c3450994f0d330a5029
Brendan Woo <woo.brendan@gmail.com>
2022-12-02 12:56:13 -0800

    import and create state for selected. create method for getCompName and pass to mappedExp

--- src/components/Experience/ExpGrid.js
+++ src/components/Experience/ExpGrid.js
@@ -1,12 +1,21 @@
 import { experience } from '../../seeds/experience';
 import ExperienceGridCard from './ExperienceGridCard';
 import '../../Styles/ExpGrid.scss';
+import { useState } from 'react'; 
 
 function ExpGrid() {
+  const [selected, setSelected] = useState('');
+
+  const getCompName = (value) => {
+    console.log('value', value)
+    setSelected(value)
+  }
   const mappedExp = experience.map((exp, index) => (
-    <section className="experience_grid">
-      <ExperienceGridCard experience={exp}/>
-    </section>
+      <ExperienceGridCard 
+        experience={exp} 
+        getCompName={getCompName}
+        key={index}
+      />
   ))
   return (
     <section className="experience_grid">
