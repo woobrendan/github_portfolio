57b452a03b675f5489412260e0b683e2ee05ecb4
Brendan Woo <woo.brendan@gmail.com>
2022-12-05 13:48:33 -0800

    import usestate, create state variable showingDetails. create meoth getDetailStatus and pass to mappedJobs

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,19 +1,26 @@
 import { Card, CardMedia, CardContent } from '@mui/material'
 import ExpPositions from './ExpPositions';
 import { Cancel } from '@mui/icons-material';
+import { useState } from 'react'
 
 
 function ExperienceCard({ experience, close }) {
 
   const positions = experience.positions
+  const [showingDetails, setShowingDetails] = useState(false)
+
+  const getDetailStatus = (value) => setShowingDetails(value) 
 
   const mappedJobs = positions.map((position, index) => (
     <ExpPositions 
       key={index } 
       position={position}
       hasToggle={positions.length > 1 ? true : false} 
+      getDetailStatus={getDetailStatus}
     />
   ))
+
+  console.log('show', showingDetails)
 
   return (
     <Card >
