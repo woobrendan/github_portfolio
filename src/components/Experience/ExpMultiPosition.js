7b14317d6fca2db99fa738684c2d3890f30b00c7
Brendan Woo <woo.brendan@gmail.com>
2022-12-06 15:47:01 -0800

    create method selectedJob and conditionally render if selectedPosition is truthy. consoldiate imports for icons

--- src/components/Experience/ExpMultiPosition.js
+++ src/components/Experience/ExpMultiPosition.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import {AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
 import { Button } from "@mui/material";
 
 
@@ -16,15 +15,40 @@ function ExpMultiPosition({ positions }) {
       <div className="details_toggle">
         <h4>Details</h4>
         <Button onClick={() => setSelectedPosition(position.title)}>         
-          <AddCircleOutlineIcon fontSize="medium" />        
+          <AddCircleOutline fontSize="medium" />        
         </Button>  
       </div>
     </>
   ))
 
+  const selectedJob = () => {
+    for (let position of positions) {
+      if (position.title === selectedPosition) {
+        return (
+          <>
+            <div className="position_header">
+              <h3>{position.title}</h3>
+              <p>{position.year}</p>
+            </div>
+            {position.duties.map((duty, index) => (
+              <li key={index}>{duty}</li>
+            ))}   
+            <div className="details_toggle">
+              <h4>Close</h4>
+              <Button onClick={() => setSelectedPosition('')}>         
+                <RemoveCircleOutline fontSize="medium" />        
+              </Button>  
+            </div>
+          </>
+        )
+      }
+    }
+  }
+
   return (
     <>
       {!selectedPosition && mappedPositions}
+      {selectedPosition && selectedJob()}
     </>
   )
 }
