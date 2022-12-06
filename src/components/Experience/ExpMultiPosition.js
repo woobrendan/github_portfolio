142f91ffe00797a47f071eed0efb991e53846303
Brendan Woo <woo.brendan@gmail.com>
2022-12-06 15:41:21 -0800

    import button component from mui. fix map function in mappedPositions. change onClick handler in button to setSelectedPositions. conditional render for selectedPosition

--- src/components/Experience/ExpMultiPosition.js
+++ src/components/Experience/ExpMultiPosition.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import { Button } from "@mui/material";
 
 
 function ExpMultiPosition({ positions }) {
   const [selectedPosition, setSelectedPosition] = useState('')
 
-  const mappedPositions = positions.mappedPositions((position, index) => (
+  const mappedPositions = positions.map((position, index) => (
     <>
       <div className="position_header">
         <h3>{position.title}</h3>
@@ -14,7 +15,7 @@ function ExpMultiPosition({ positions }) {
       </div>   
       <div className="details_toggle">
         <h4>Details</h4>
-        <Button onClick={()=> handleToggle()}>         
+        <Button onClick={() => setSelectedPosition(position.title)}>         
           <AddCircleOutlineIcon fontSize="medium" />        
         </Button>  
       </div>
@@ -23,21 +24,8 @@ function ExpMultiPosition({ positions }) {
 
   return (
     <>
-    <div className="position_header">
-      <h3>{position.title}</h3>
-      <p>{position.year}</p>
-    </div>   
-    <div className="details_toggle">
-      <h4>Details</h4>
-      <Button onClick={()=> handleToggle()}>
-        {selectedPosition
-          ? <RemoveCircleOutlineIcon fontSize="medium" /> 
-          : <AddCircleOutlineIcon fontSize="medium" /> 
-        }
-      </Button>  
-    </div>
-
-   </>
+      {!selectedPosition && mappedPositions}
+    </>
   )
 }
 
