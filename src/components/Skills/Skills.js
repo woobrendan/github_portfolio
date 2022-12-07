79297382f307baf25df190f0551920df40eccf63
Brendan Woo <woo.brendan@gmail.com>
2022-12-07 12:12:59 -0800

    change set width of card to 420

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,7 +8,7 @@ function Skills() {
   return (
     <section id="skills-list">
       {skills.map((skill, index) => (
-        <Card key={index} sx={{ width: 475, minHeight: 275 }}>
+        <Card key={index} sx={{ width: 420, minHeight: 275 }}>
           <CardContent>
             <div className="skill_card" key={skill.title}>
               <h2>{skill.title}</h2>
