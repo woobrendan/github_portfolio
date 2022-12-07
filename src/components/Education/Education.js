72fe0b5c8bed4c3adeac69c163a3755c1056867d
Brendan Woo <woo.brendan@gmail.com>
2022-12-07 13:55:29 -0800

    fix indentation and consolidate education card props

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,18 +2,14 @@ import education from '../../seeds/education.json'
 import EducationCard from './EducationCard';
 import '../../Styles/education.scss'
 
-function Education() {
-  
+function Education() { 
   return (
     <>
       <h1>Education</h1>
-        <div className="education_card_list">
-          {education.map((school) => (
-            <EducationCard 
-              schoolDetails={school} 
-              key={school.schoolName}
-            />
-          ))}
+      <div className="education_card_list">
+        {education.map((school) => (
+          <EducationCard schoolDetails={school} key={school.schoolName} />
+        ))}
       </div>
     </>
   )
