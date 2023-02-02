19b46d25c50d475cbf0a574c3930650f1c41e712
Brendan Woo <woo.brendan@gmail.com>
2023-02-02 13:16:03 -0800

    remove work highlight h3 from component

--- src/components/About/ImageSlider.js
+++ src/components/About/ImageSlider.js
@@ -1,35 +1,31 @@
-import { sliderData } from "../../seeds/images"
-import { useState } from "react"
-import '../../Styles/slider.scss'
+import { sliderData } from "../../seeds/images";
+import { useState } from "react";
+import "../../Styles/slider.scss";
 
 const ImageSlider = () => {
   const [current, setCurrent] = useState(0);
-  const length = sliderData.length
+  const length = sliderData.length;
 
   const incrementCount = () => {
     setTimeout(() => {
-      setCurrent(current === length - 1 ? 0 : current + 1)
-    }, 4000)
-  }
+      setCurrent(current === length - 1 ? 0 : current + 1);
+    }, 4000);
+  };
 
-  incrementCount()
+  incrementCount();
 
   const mappedImages = sliderData.map((company, index) => (
-    <div 
-      className={index === current ? 'slide_active' : 'slide'}
+    <div
+      className={index === current ? "slide_active" : "slide"}
       key={company.name}
     >
-      {index === current && <img src={company.image} alt={company.name} className="image" />}
-
+      {index === current && (
+        <img src={company.image} alt={company.name} className="image" />
+      )}
     </div>
-  ))
+  ));
 
-  return (
-    <section className="slider">
-      <h3>Work Highlights</h3>
-      {mappedImages}
-    </section>  
-  )
-}
+  return <section className="slider">{mappedImages}</section>;
+};
 
-export default ImageSlider
+export default ImageSlider;
