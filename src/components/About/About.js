55418edfad6d18227ab65b5e1ba11c2e08c27433
Brendan Woo <woo.brendan@gmail.com>
2023-02-02 13:00:20 -0800

    remove inline styling for box img component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,19 @@
-import '../../Styles/about.scss'
-import Box from '@mui/material/Box';
-import pwc from '../../images/PWC.jpg'
-import ImageSlider from './ImageSlider';
-import AboutDescription from './AboutDescription';
+import "../../Styles/about.scss";
+import Box from "@mui/material/Box";
+import pwc from "../../images/PWC.jpg";
+import ImageSlider from "./ImageSlider";
+import AboutDescription from "./AboutDescription";
 
 export default function About() {
   return (
     <section id="About">
       <div className="intro_header">
         <div className="intro_left">
-          <Box
-            component="img"
-            sx={{ width: 300, height: 300 }}
-            alt="SRO Portrait"
-            src={pwc}
-            id="About-img"
-          />
+          <Box component="img" alt="SRO Portrait" src={pwc} id="About-img" />
           <ImageSlider />
         </div>
         <AboutDescription />
       </div>
     </section>
-  )
+  );
 }
