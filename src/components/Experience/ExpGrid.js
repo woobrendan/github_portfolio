f0485ac7d23f950d63ac053d744bd16a8a362bb0
Brendan Woo <woo.brendan@gmail.com>
2023-02-03 16:45:32 -0800

    prettier adjustment

--- src/components/Experience/ExpGrid.js
+++ src/components/Experience/ExpGrid.js
@@ -1,32 +1,34 @@
-import { experience } from '../../seeds/experience';
-import ExperienceGridCard from './ExperienceGridCard';
-import ExperienceCard from './ExperienceCard';
-import '../../Styles/ExpGrid.scss';
-import { useState } from 'react'; 
+import { experience } from "../../seeds/experience";
+import ExperienceGridCard from "./ExperienceGridCard";
+import ExperienceCard from "./ExperienceCard";
+import "../../Styles/ExpGrid.scss";
+import { useState } from "react";
 
 function ExpGrid() {
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState("");
 
-  const getCompName = (value) => setSelected(value)
+  const getCompName = (value) => setSelected(value);
 
   const mappedExp = experience.map((exp, index) => (
-    <ExperienceGridCard 
-      experience={exp} 
+    <ExperienceGridCard
+      experience={exp}
       getCompName={getCompName}
       key={index}
     />
-  ))
+  ));
 
   const selectedExpCard = () => {
-    const selectedJob = experience.find(job => job.company === selected)
-    return <ExperienceCard experience={selectedJob} close={() => setSelected('')} />
-  }
+    const selectedJob = experience.find((job) => job.company === selected);
+    return (
+      <ExperienceCard experience={selectedJob} close={() => setSelected("")} />
+    );
+  };
 
   return (
     <section className="experience_grid">
       {selected ? selectedExpCard() : mappedExp}
     </section>
-  )
+  );
 }
 
-export default ExpGrid 
+export default ExpGrid;
