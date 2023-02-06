612a16f317caa343cd5f9b4f0cd0058c52417b70
Brendan Woo <woo.brendan@gmail.com>
2023-02-06 11:35:09 -0800

    create usestate for isToggled, and set the value within new method getToggledStatus

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,19 +1,28 @@
-import { Card, CardMedia, CardContent } from '@mui/material'
-import { Cancel } from '@mui/icons-material';
-import ExpPositionNoToggle from './ExpPositionNoToggle';
-import ExpMultiPosition from './ExpMultiPosition';
-import irb from '../../images/IRB.jpeg'
-
+import { Card, CardMedia, CardContent } from "@mui/material";
+import { Cancel } from "@mui/icons-material";
+import ExpPositionNoToggle from "./ExpPositionNoToggle";
+import ExpMultiPosition from "./ExpMultiPosition";
+import irb from "../../images/IRB.jpeg";
+import { useState } from "react";
 
 function ExperienceCard({ experience, close }) {
+  const [isToggled, setIsToggled] = useState(false);
+  const positions = experience.positions;
+  const getImage =
+    experience.company !== "Immigration Refugee Board of Canada"
+      ? experience.image
+      : irb;
 
-  const positions = experience.positions
-  const getImage = experience.company !== 'Immigration Refugee Board of Canada' ? experience.image : irb
+  const getToggleStatus = (value) => {
+    if (value) {
+      setIsToggled(true);
+    }
+  };
 
   return (
-    <Card >
-      <Cancel size="large" onClick={() => close()} className="toggle_close"/>
-       <CardMedia
+    <Card>
+      <Cancel size="large" onClick={() => close()} className="toggle_close" />
+      <CardMedia
         component="img"
         image={getImage}
         alt={experience.company}
@@ -23,14 +32,15 @@ function ExperienceCard({ experience, close }) {
         <h2>{experience.company}</h2>
         <h4>Location: {experience.location}</h4>
         <div className="work-info">
-          {positions.length <= 1 
-            ? <ExpPositionNoToggle position={positions[0]} />
-            : <ExpMultiPosition positions={positions}/>
-          }
-        </div>           
+          {positions.length <= 1 ? (
+            <ExpPositionNoToggle position={positions[0]} />
+          ) : (
+            <ExpMultiPosition positions={positions} />
+          )}
+        </div>
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export default ExperienceCard
+export default ExperienceCard;
