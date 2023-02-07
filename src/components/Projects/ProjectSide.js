650f59547d941095bf4d1c00029090668102a58b
Brendan Woo <woo.brendan@gmail.com>
2023-02-07 10:59:33 -0800

    change ternerary class name to be just projet_side_view without number. remove fragment

--- src/components/Projects/ProjectSide.js
+++ src/components/Projects/ProjectSide.js
@@ -1,15 +1,15 @@
-import { Button } from '@mui/material'
-import GitHubIcon from '@mui/icons-material/GitHub';
+import { Button } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function ProjectSide(props) {
-  const project = props.project
-  const isEven = props.index % 2 === 0 
+  const project = props.project;
+  const isEven = props.index % 2 === 0;
 
   const projectImg = (
     <div className="project_img">
       <img src={project.gif} alt={project.name} />
     </div>
-  )
+  );
   const projectDetails = (
     <div className="project_info">
       <h5>{project.name}</h5>
@@ -19,16 +19,14 @@ function ProjectSide(props) {
         Code
       </Button>
     </div>
-  )
+  );
 
   return (
-    <section className={isEven ? 'project_side_view_2' : 'project_side_view_1'}>
-      {!isEven 
-        ? <>{projectImg}{projectDetails}</>
-        : <>{projectDetails}{projectImg}</>
-      }
+    <section className="project_side_view">
+      {projectImg}
+      {projectDetails}
     </section>
-  )
+  );
 }
 
-export default ProjectSide
+export default ProjectSide;
