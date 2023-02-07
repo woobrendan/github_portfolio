fbe90cbdebd16c9516088b32b9e384869733db74
Brendan Woo <woo.brendan@gmail.com>
2023-02-07 11:11:09 -0800

    move blocks from proejctImg and ProjectInfo inside return component instead of dedicating to memory

--- src/components/Projects/ProjectSide.js
+++ src/components/Projects/ProjectSide.js
@@ -4,27 +4,19 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 const ProjectSide = ({ project }) => {
   const { name, description, frameworks, link, gif } = project;
 
-  const projectImg = (
-    <div className="project_img">
-      <img src={gif} alt={name} />
-    </div>
-  );
-
-  const projectDetails = (
-    <div className="project_info">
-      <h5>{name}</h5>
-      <p>{description}</p>
-      <p>Frameworks: {frameworks}</p>
-      <Button variant="outlined" href={link} startIcon={<GitHubIcon />}>
-        Code
-      </Button>
-    </div>
-  );
-
   return (
     <section className="project_side_view">
-      {projectImg}
-      {projectDetails}
+      <div className="project_img">
+        <img src={gif} alt={name} />
+      </div>
+      <div className="project_info">
+        <h5>{name}</h5>
+        <p>{description}</p>
+        <p>Frameworks: {frameworks}</p>
+        <Button variant="outlined" href={link} startIcon={<GitHubIcon />}>
+          Code
+        </Button>
+      </div>
     </section>
   );
 };
