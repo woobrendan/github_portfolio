95c9b009eca0498f222ce90070bf61dadce32934
Brendan Woo <woo.brendan@gmail.com>
2023-02-07 11:09:10 -0800

    remove index prop for projectSide. change key to be project.name and remove index paramter from map

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -1,21 +1,15 @@
-import { projects } from '../../seeds/projectInfo.js'
-import ProjectSide from './ProjectSide.js'
-import '../../Styles/project.scss'
+import { projects } from "../../seeds/projectInfo.js";
+import ProjectSide from "./ProjectSide.js";
+import "../../Styles/project.scss";
 
-function ProjectList() {
-
+const ProjectList = () => {
   return (
-      <section id="project-list" >
-        {projects.map((project, index) => (
-          <ProjectSide
-            project={project} 
-            key={index}
-            index={index + 1}
-          />
-        ))}
-      </section>
-  )
-}
+    <section id="project-list">
+      {projects.map((project) => (
+        <ProjectSide project={project} key={project.name} />
+      ))}
+    </section>
+  );
+};
 
-export default ProjectList
-
+export default ProjectList;
