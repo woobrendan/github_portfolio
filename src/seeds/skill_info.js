ce55287af3c51ff43ab916e39c7b9d8e0d0d2494
Brendan Woo <woo.brendan@gmail.com>
2023-02-09 11:01:51 -0800

    add typescript to language skills

--- src/seeds/skill_info.js
+++ src/seeds/skill_info.js
@@ -1,14 +1,26 @@
 export const skills = [
   {
-    title: "Languages", 
-    info: ["JavaScript", "Ruby", "HTML", "CSS", "SQL"]
+    title: "Languages",
+    info: ["Typescript", "JavaScript", "Ruby", "HTML", "CSS", "SQL"],
   },
   {
     title: "Testing",
-    info: ["Cypress", "Jest", "Storybook", "Mocha"]
+    info: ["Cypress", "Jest", "Storybook", "Mocha"],
   },
   {
-    title: "Frameworks/Libraries", 
-    info: ["React", "Node.js", "MongoDB", "Material UI",  "Express", "jQuery", "Bootstrap", "SASS", "Rails", "Active Record", "PostgreSQL"]
-  }
-]
+    title: "Frameworks/Libraries",
+    info: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Material UI",
+      "Express",
+      "jQuery",
+      "Bootstrap",
+      "SASS",
+      "Rails",
+      "Active Record",
+      "PostgreSQL",
+    ],
+  },
+];
