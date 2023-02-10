14a1d1743878868dec6a170d344c75171180a140
Brendan Woo <woo.brendan@gmail.com>
2023-02-10 11:01:23 -0800

    change year of all experience from full 4 digits to short form 2 digits

--- src/seeds/experience.js
+++ src/seeds/experience.js
@@ -1,7 +1,7 @@
-import tbj from '../images/TBJ.jpeg';
-import sro from '../images/sro.jpeg'
-import freedom from '../images/freedom.png';
-import irb2 from '../images/irb2.png';
+import tbj from "../images/TBJ.jpeg";
+import sro from "../images/sro.jpeg";
+import freedom from "../images/freedom.png";
+import irb2 from "../images/irb2.png";
 
 export const experience = [
   {
@@ -11,34 +11,34 @@ export const experience = [
     positions: [
       {
         title: "Team Relations Coordinator",
-        year: "Oct 2016 - Oct 2022",
+        year: "Oct '16 - Oct '22",
         duties: [
           "Maintain constant communication with clients and partners through phone calls, meetings and emails to ensure strong relationships and timely assistance",
           "Ensure the success of events by planning and managing operations, delegating tasks for prompt setup",
           "Prepare various official documents and coordinate their distribution to constituents",
           "Advise on marketing plans for partnership activations, such as branding, car corrals, car parades, autograph sessions, and more, improving the overall fan experience",
-          "Manage budget for vendors and operations, reducing spending for each season"
-        ]
+          "Manage budget for vendors and operations, reducing spending for each season",
+        ],
       },
       {
         title: "Video Technician",
-        year: "Jan 2016 - Oct 2016",
+        year: "Jan '16 - Oct '16",
         duties: [
           "Created a new process to structure and expedite video submissions, reducing wait times and increasing efficiency by 29%",
           "Coordinated in-car cameras with all competition teams to ensure their compliance with sporting regulations and with marketing and TV requirements",
-          "Acquired in-car video for processing, reacting to changing demands of competition stewards and TV production crew"
-        ]
+          "Acquired in-car video for processing, reacting to changing demands of competition stewards and TV production crew",
+        ],
       },
       {
         title: "Marketing & Communications Intern",
-        year: "May 2014 - Dec 2015",
+        year: "May '14 - Dec '15",
         duties: [
           "Ensured successful hosting of racing events throughout North America by collaborating with the Marketing & Communications team",
           "Assisted the migration to more modern marketing practices through social media platforms resulting in increased awareness and social traffic",
-          "Facilitated the distribution of marketing materials to both teams and the public, including over 500 copies of print media per event"
-        ]
-      }
-    ]
+          "Facilitated the distribution of marketing materials to both teams and the public, including over 500 copies of print media per event",
+        ],
+      },
+    ],
   },
   {
     company: "Immigration Refugee Board of Canada",
@@ -47,15 +47,14 @@ export const experience = [
     positions: [
       {
         title: "Assistant to Coordinating Member",
-        year: "Sept 2021 - Dec 2021",
+        year: "Sept '21 - Dec '21",
         duties: [
           "Worked a casual contract supporting multiple teams and managers within the Refugee Protection Division (RPD)",
           "Coordinated schedules for various managers, ensuring organized days and meetings",
-          "Facilitated member hearing schedules and caseloads, updating HR software as required"
-        ]
-      }
-      
-    ]
+          "Facilitated member hearing schedules and caseloads, updating HR software as required",
+        ],
+      },
+    ],
   },
   {
     company: "Toronto Blue Jays",
@@ -64,15 +63,14 @@ export const experience = [
     positions: [
       {
         title: "Warehouse Assistant",
-        year: "Feb 2018 - Nov 2018",
+        year: "Feb '18 - Nov '18",
         duties: [
           "Coordinated the movement of all Blue Jays product, receiving and distributing to various vendor locations throughout the stadium and stores",
           "Maintained multiple warehouses to ensure cleanliness and inventory accuracy",
-          "Re-organized a warehouse to a new organizational style leading to time saved"
-        ]
-      }
-      
-    ]
+          "Re-organized a warehouse to a new organizational style leading to time saved",
+        ],
+      },
+    ],
   },
   {
     company: "Freedom Mobile",
@@ -81,14 +79,12 @@ export const experience = [
     positions: [
       {
         title: "Freedom Specialist",
-        year: "Nov 2014 - Jan 2017",
+        year: "Nov '14 - Jan '17",
         duties: [
           "Consistently met monthly sales goals while delivering exceptional customer service",
-          "Trained new employees to better communicate and sell Wind services and how to properly manage money and sales"
-        ]
-      }
-      
-    ]
-  }
-]
-
+          "Trained new employees to better communicate and sell Wind services and how to properly manage money and sales",
+        ],
+      },
+    ],
+  },
+];
