4d4ec1bd93cb642c815f7c4e656770cd02ad09e8
Brendan Woo <woo.brendan@gmail.com>
2023-07-24 13:58:51 -0400

    update experience bullets

--- src/seeds/experience.js
+++ src/seeds/experience.js
@@ -4,87 +4,87 @@ import freedom from "../images/freedom.png";
 import irb2 from "../images/irb2.png";
 
 export const experience = [
-  {
-    company: "SRO Motorsports America",
-    location: "Various, USA",
-    image: sro,
-    positions: [
-      {
-        title: "Team Relations Coordinator",
-        year: "Oct '16 - Oct '22",
-        duties: [
-          "Maintain constant communication with clients and partners through phone calls, meetings and emails to ensure strong relationships and timely assistance",
-          "Ensure the success of events by planning and managing operations, delegating tasks for prompt setup",
-          "Prepare various official documents and coordinate their distribution to constituents",
-          "Advise on marketing plans for partnership activations, such as branding, car corrals, car parades, autograph sessions, and more, improving the overall fan experience",
-          "Manage budget for vendors and operations, reducing spending for each season",
+    {
+        company: "SRO Motorsports America",
+        location: "Various, USA",
+        image: sro,
+        positions: [
+            {
+                title: "Team Relations Coordinator",
+                year: "Oct '16 - Oct '22",
+                duties: [
+                    "Provide expert advice on marketing strategies for partnership activations, including initiatives such as branding, car corrals, car parades, autograph sessions, and more, contributing to the enhancement of the overall fan experience",
+                    "Engage in effective communication with clients and partners, fostering strong relationships and ensuring their needs are consistently met",
+                    "Ensure the success of events by planning and managing operations, through leading the event worker team for prompt setup",
+                    "Prepare various official documents and coordinate their distribution to constituents",
+                    "Manage budget for vendors and operations, reducing spending for each season",
+                ],
+            },
+            {
+                title: "Video Technician",
+                year: "Jan '16 - Oct '16",
+                duties: [
+                    "Created a new process to structure and expedite video submissions, reducing wait times and increasing efficiency by 29%",
+                    "Coordinated in-car cameras with all competition teams to ensure their compliance with sporting regulations and with marketing and TV requirements",
+                    "Acquired in-car video for processing, reacting to changing demands of competition stewards and TV production crew",
+                ],
+            },
+            {
+                title: "Marketing & Communications Intern",
+                year: "May '14 - Dec '15",
+                duties: [
+                    "Ensured successful hosting of racing events throughout North America by collaborating with the Marketing & Communications team",
+                    "Assisted the migration to more modern marketing practices through social media platforms resulting in increased awareness and social traffic",
+                    "Facilitated the distribution of marketing materials to both teams and the public, including over 500 copies of print media per event",
+                ],
+            },
         ],
-      },
-      {
-        title: "Video Technician",
-        year: "Jan '16 - Oct '16",
-        duties: [
-          "Created a new process to structure and expedite video submissions, reducing wait times and increasing efficiency by 29%",
-          "Coordinated in-car cameras with all competition teams to ensure their compliance with sporting regulations and with marketing and TV requirements",
-          "Acquired in-car video for processing, reacting to changing demands of competition stewards and TV production crew",
+    },
+    {
+        company: "Immigration Refugee Board of Canada",
+        location: "Toronto, ON",
+        image: irb2,
+        positions: [
+            {
+                title: "Assistant to Coordinating Member",
+                year: "Sept '21 - Dec '21",
+                duties: [
+                    "Successfully fulfilled a dynamic casual contract, providing dedicated support to numerous teams and managers within the Refugee Protection Division (RPD)",
+                    "Exemplified exceptional organizational skills in efficiently coordinating and managing managers' schedules, ensuring seamless and well-structured days, and arranging meetings to optimize productivity and collaboration",
+                    "Demonstrated a high level of professionalism and proficiency in skillfully facilitating member hearing schedules and caseloads, meticulously updating HR software as needed to maintain accurate and up-to-date records",
+                ],
+            },
         ],
-      },
-      {
-        title: "Marketing & Communications Intern",
-        year: "May '14 - Dec '15",
-        duties: [
-          "Ensured successful hosting of racing events throughout North America by collaborating with the Marketing & Communications team",
-          "Assisted the migration to more modern marketing practices through social media platforms resulting in increased awareness and social traffic",
-          "Facilitated the distribution of marketing materials to both teams and the public, including over 500 copies of print media per event",
+    },
+    {
+        company: "Toronto Blue Jays",
+        location: "Toronto, ON",
+        image: tbj,
+        positions: [
+            {
+                title: "Warehouse Assistant",
+                year: "Feb '18 - Nov '18",
+                duties: [
+                    "Coordinated the movement of all Blue Jays product, receiving and distributing to various vendor locations throughout the stadium and stores",
+                    "Maintained multiple warehouses to ensure cleanliness and inventory accuracy",
+                    "Re-organized a warehouse to a new organizational style leading to time saved",
+                ],
+            },
         ],
-      },
-    ],
-  },
-  {
-    company: "Immigration Refugee Board of Canada",
-    location: "Toronto, ON",
-    image: irb2,
-    positions: [
-      {
-        title: "Assistant to Coordinating Member",
-        year: "Sept '21 - Dec '21",
-        duties: [
-          "Worked a casual contract supporting multiple teams and managers within the Refugee Protection Division (RPD)",
-          "Coordinated schedules for various managers, ensuring organized days and meetings",
-          "Facilitated member hearing schedules and caseloads, updating HR software as required",
+    },
+    {
+        company: "Freedom Mobile",
+        location: "Oshawa, ON",
+        image: freedom,
+        positions: [
+            {
+                title: "Freedom Specialist",
+                year: "Nov '14 - Jan '17",
+                duties: [
+                    "Consistently met monthly sales goals while delivering exceptional customer service",
+                    "Trained new employees to better communicate and sell Wind services and how to properly manage money and sales",
+                ],
+            },
         ],
-      },
-    ],
-  },
-  {
-    company: "Toronto Blue Jays",
-    location: "Toronto, ON",
-    image: tbj,
-    positions: [
-      {
-        title: "Warehouse Assistant",
-        year: "Feb '18 - Nov '18",
-        duties: [
-          "Coordinated the movement of all Blue Jays product, receiving and distributing to various vendor locations throughout the stadium and stores",
-          "Maintained multiple warehouses to ensure cleanliness and inventory accuracy",
-          "Re-organized a warehouse to a new organizational style leading to time saved",
-        ],
-      },
-    ],
-  },
-  {
-    company: "Freedom Mobile",
-    location: "Oshawa, ON",
-    image: freedom,
-    positions: [
-      {
-        title: "Freedom Specialist",
-        year: "Nov '14 - Jan '17",
-        duties: [
-          "Consistently met monthly sales goals while delivering exceptional customer service",
-          "Trained new employees to better communicate and sell Wind services and how to properly manage money and sales",
-        ],
-      },
-    ],
-  },
+    },
 ];
